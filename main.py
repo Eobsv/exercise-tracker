e55f3b9bec8c2e50c4083191cc16c2b82377cc4f
Eobs <michal.kunkel@gmail.com>
2023-08-04 23:37:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+GENDER = "male"
+WEIGHT_KG = 80
+HEIGHT_CM = 183
+AGE = 26
+
+URL_CALC_FROM_NATURAL_LNG = "https://trackapi.nutritionix.com/v2/natural/exercise" # method POST
+
+headers = {
+    "x-app-key": API_KEY,
+    "x-app-id": APP_ID
+}
+
+user_input = str(input("What have you exercised?"))
+
+PARAMS = {
+    "query": user_input,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+
+response = requests.post(URL_CALC_FROM_NATURAL_LNG, headers=headers, json=PARAMS)
+
+print(response.json())
